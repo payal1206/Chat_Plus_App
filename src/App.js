468e6da8e37424647a5441a4fcd658fd20919d0b
payal1206 <68314999+payal1206@users.noreply.github.com>
2021-10-11 23:30:26 +0530

    added dashboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Dashboard from "./component/dashboard/dashboard";
 
 function App() {
-  return <div className="App">come here</div>;
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/component/dashboard/dashboard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Paper } from "@material-ui/core";
+
+export default function Dashboard() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#0000ff",
+        width: "100%",
+        height: "750px",
+      }}
+    >
+      <Paper>
+        <div></div>
+        <div></div>
+        <div></div>
+      </Paper>
+    </div>
+  );
+}

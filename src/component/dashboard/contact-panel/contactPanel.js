547dc2ee4e3eeedd3b7fdecc926df063f49d5d88
Ofoli <lawrenceofoli@gmail.com>
2021-10-13 15:46:08 +0000

    created dashboard template

---
+++ src/component/dashboard/contact-panel/contactPanel.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "../dashboard.module.css";
+
+function ContactPanel() {
+  return <div className={classes.contactPanelDiv}>Contact panel</div>;
+}
+
+export default ContactPanel;
--- src/component/dashboard/dashboard.js
+++ src/component/dashboard/dashboard.js
@@ -1,8 +1,26 @@
 import React from "react";
-
-import { Paper } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import classes from "./dashboard.module.css";
 
 import Layout from "./layout/layout";
+import SidePanel from "./side-panel/sidePanel";
+import MessagePanel from "./message-panel/messagePanel";
+import ContactPanel from "./contact-panel/contactPanel";
+
 export default function Dashboard() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <Grid container style={{ height: "100%" }}>
+        <Grid item lg={2} className={classes.sidePanel}>
+          <SidePanel />
+        </Grid>
+        <Grid item lg={3} className={classes.contactPanel}>
+          <ContactPanel />
+        </Grid>
+        <Grid item lg={7} className={classes.messagePanel}>
+          <MessagePanel />
+        </Grid>
+      </Grid>
+    </Layout>
+  );
 }

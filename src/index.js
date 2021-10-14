8eb188bbf4fe9eb26fbb8d14f5d0f13d392d8e27
payal1206 <68314999+payal1206@users.noreply.github.com>
2021-10-15 01:13:40 +0530

    signincorrect  page

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import Reducer from "./component/redux-store/reducers/reducers";
+
+const store = createStore(
+  Reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );

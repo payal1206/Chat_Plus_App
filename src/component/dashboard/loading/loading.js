5d8d40403723c7ce34da85482188b6af00977da3
payal1206 <68314999+payal1206@users.noreply.github.com>
2021-11-01 21:55:22 +0530

    added loading page

--- src/component/dashboard/loading/loading.js
+++ src/component/dashboard/loading/loading.js
@@ -4,15 +4,25 @@ import CircularProgress from "@mui/material/CircularProgress";
 
 export default function CircularColor() {
   return (
-    <Stack
-      sx={{ color: "grey.500" }}
-      spacing={5}
-      direction="row"
-      padding="200px"
-    >
-      <CircularProgress color="secondary" />
-      <CircularProgress color="success" />
-      <CircularProgress color="inherit" />
-    </Stack>
+    <>
+      <Stack
+        sx={{ color: "grey.600" }}
+        spacing={5}
+        direction="row"
+        padding="200px"
+        display="flex"
+      >
+        <CircularProgress color="secondary" />
+        <CircularProgress color="success" />
+        <CircularProgress color="inherit" />
+      </Stack>
+      {/* <p
+        style={{
+          paddingBottom: "200px",
+        }}
+      >
+        <b>Fetching Results</b>
+      </p> */}
+    </>
   );
 }

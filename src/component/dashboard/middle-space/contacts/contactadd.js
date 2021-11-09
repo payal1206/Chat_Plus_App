d512e126bf1c3ab2dfe0e864f7a1fd4195e750af
Ofoli <lawrenceofoli@gmail.com>
2021-11-09 20:49:46 +0000

    corrected middle space styling

--- src/component/dashboard/middle-space/contacts/contactadd.js
+++ src/component/dashboard/middle-space/contacts/contactadd.js
@@ -1,22 +1,20 @@
 import React from "react";
-import List from "@mui/material/List";
+
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import AddForm from "./addform";
-
-import EmailIcon from "@mui/icons-material/Email";
-import ChatIcon from "@mui/icons-material/Chat";
-import GroupWorkIcon from "@mui/icons-material/GroupWork";
-import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 
 import GroupIcon from "@mui/icons-material/Group";
 const bkcol = "rgb(161, 174, 187)";
 
 const Contactadd = (props) => {
   return (
-    <ListItem disablePadding onClick={props.handleClick}>
+    <ListItem
+      disablePadding
+      onClick={props.handleClick}
+      sx={{ height: "70px" }}
+    >
       <ListItemButton>
         <ListItemIcon>
           <GroupIcon style={{ color: bkcol }} />
